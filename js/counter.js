3f97b6ecbdcc34ff794cca3395fba374a5672216
chestersoriano <109785347+chestersoriano@users.noreply.github.com>
2022-08-02 13:50:53 +0800

    Add js file for the counter

---
+++ js/counter.js
@@ -0,0 +1,21 @@
+//#region - start of - number counter animation
+const counterAnim = (qSelector, start = 0, end, duration = 1000) => {
+    const target = document.querySelector(qSelector);
+    let startTimestamp = null;
+    const step = (timestamp) => {
+     if (!startTimestamp) startTimestamp = timestamp;
+     const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+     target.innerText = Math.floor(progress * (end - start) + start);
+     if (progress < 1) {
+      window.requestAnimationFrame(step);
+     }
+    };
+    window.requestAnimationFrame(step);
+   };
+   //#endregion - end of - number counter animation
+   
+   document.addEventListener("DOMContentLoaded", () => {
+    counterAnim("#counter1", 1, 9, 700);
+    counterAnim("#counter2", 1, 20, 700);
+   });
+   
