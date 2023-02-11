9bf35c79bd3364f2d24fe91fa151410eb5b16e1b
chestersoriano <109785347+chestersoriano@users.noreply.github.com>
2023-02-11 21:23:39 +0800

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -2,20 +2,23 @@
 const counterAnim = (qSelector, start = 0, end, duration = 1000) => {
     const target = document.querySelector(qSelector);
     let startTimestamp = null;
+    
     const step = (timestamp) => {
      if (!startTimestamp) startTimestamp = timestamp;
      const progress = Math.min((timestamp - startTimestamp) / duration, 1);
      target.innerText = Math.floor(progress * (end - start) + start);
+
      if (progress < 1) {
       window.requestAnimationFrame(step);
      }
     };
     window.requestAnimationFrame(step);
    };
+
    //#endregion - end of - number counter animation
    
    document.addEventListener("DOMContentLoaded", () => {
     counterAnim("#counter1", 1, 9, 700);
-    counterAnim("#counter2", 1, 20, 700);
+    counterAnim("#counter2", 1, 22, 700);
    });
    
